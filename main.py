dd5172a1e4ac2a95c2be273248d67d7f2308dbfe
Esrael Mekdem <87760858+Ezzy401k@users.noreply.github.com>
2024-02-24 15:59:42 +0300

    Update main.py
    rounding down was added

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 print('Welcome to the tip calculator.')
 # input the price
 total_bill = float(input("What was the total bill?\n"))
-# input the perccenatge tip
+# input the percentage tip
 percentage = int(input("What percentage tip would you like to give? 10%, 12% or 15%?\n"))
-# input to how many ways to split the price
+# Input to how many ways to split the price
 split = int(input("How many people to split the bill?\n"))
 # calculate the percentage tip
 tip = total_bill * (percentage/100)
-# the new total price with the tip
+# The new total price with the tip
 total_price = total_bill + tip
-# individual pay amount
-individual_pay = round(total_price/split,2)
+# Individual pay amount rounded to 2 decimal numbers
+individual_pay = "{:.2f}".format(total_price/split)
 print(f"Each person should pay: {individual_pay}")
